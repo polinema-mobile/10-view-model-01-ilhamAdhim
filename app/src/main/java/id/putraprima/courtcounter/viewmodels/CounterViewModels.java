261f2cd9bd187debcd81514c9f8bd7823b7b29d8
ilhamadhim <ilhamm179@gmail.com>
2020-10-27 16:21:50 +0700

    Added Reset Score function

--- app/src/main/java/id/putraprima/courtcounter/viewmodels/CounterViewModels.java
+++ app/src/main/java/id/putraprima/courtcounter/viewmodels/CounterViewModels.java
@@ -30,4 +30,12 @@ public class CounterViewModels extends ViewModel {
     }
 
     //TODO : Tambahkan Implementasi View Model Untuk Reset Point
+    public void resetScore(){
+        score.setHomeScore(0);
+        score.setAwayScore(0);
+        scoreMutableLiveData.setValue(score);
+        Log.d("Reset,Point Away : ", String.valueOf(score.getAwayScore()));
+        Log.d("Point Home : ", String.valueOf(score.getHomeScore()));
+
+    }
 }
